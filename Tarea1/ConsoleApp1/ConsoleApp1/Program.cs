cae765a35b351699001976c1e464e5227e7d0a4e
Starling Germosen <sgermosen@outlook.com>
2025-02-01 09:38:30 -0400

    Subiendo la tarea 1

--- Tarea1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Tarea1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+AssemblyDescriptionAttributesd
+    asd
+    as
+    decimalasd
+    as
+    DataMisalignedExceptionsd
+    a
+    sd
+    asd
+    as
+    decimalasd
